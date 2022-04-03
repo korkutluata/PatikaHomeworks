5dfe290cfbc2bf5303a1e9aecae6f1bf5e034c47
korkutluata <korkutkutluata@gmail.com>
2022-04-03 11:30:52 +0300

    if else if ternary eklendi

---
+++ C#_Odevler/If_ElseIf_Ternary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace If_ElseIf_Ternary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Günaydın !");
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("İyi Günler !");
+            }
+            else
+            {
+                Console.WriteLine("İyi Geceler !");
+            }
+                
+                // ternary kismimiz
+            string sonuc = time <= 18 ? "İyi Günler !" : "İyi Geceler !";
+            sonuc = time >= 6 && time < 11 ? "Günaydın !" : time <= 18 ? "İyi Günler !" : "İyi Geceler !";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
