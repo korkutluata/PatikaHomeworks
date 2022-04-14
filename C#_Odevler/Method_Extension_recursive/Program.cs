d55df73ec301ab8fcc0130fac579d4c00db2abbf
korkutluata <korkutkutluata@gmail.com>
2022-04-14 15:09:55 +0300

    Method_Extension_recursive eklendi

---
+++ C#_Odevler/Method_Extension_recursive/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Method_Extension_recursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Recursive Metotlar - Öz Yinelemeli
+
+            // 3^4 islemini yapiyoruz
+            int result = 1;
+            for (int i = 1; i < 5; i++)
+            {
+                result = result * 3;
+            }
+            Console.WriteLine(result);
+
+            Islemler instance = new();
+            Console.WriteLine(instance.Expo(3, 4));
+
+            //Extension Metotlar
+            string ifade = "Korkut Kutlu Ata";
+            bool sonuc = ifade.CheckSpaces();
+            Console.WriteLine(sonuc);
+            if (sonuc)
+            {
+                Console.WriteLine(ifade.RemoveWhiteSpaces());
+            }
+            Console.WriteLine(ifade.MakeUpperCase());
+            Console.WriteLine(ifade.MakeLowerCase());
+
+            int[] dizi = { 9, 3, 6, 2, 1, 5, 0 };
+            dizi.SortArray();
+            dizi.EkranaYazdir();
+
+            int sayi = 5;
+            Console.WriteLine(sayi.IsEvenNumber());
+
+            Console.WriteLine(ifade.GetFirstCharacter());
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Islemler
+    {
+        // recursive method ornegi
+        public int Expo(int sayi, int us)    
+        {
+            if (us < 2)
+            {
+                return sayi;
+            }
+            return Expo(sayi, us - 1) * sayi;
+        }
+        //Expo(3,4)
+        //Expo(3,3) * 3;
+        //Expo(3,2) * 3 * 3;
+        //Expo(3,1) * 3 * 3 * 3;
+        // 3 * 3 * 3 * 3 = 3 ^ 4;
+    }
+
+    public static class Extension
+    {
+        public static bool CheckSpaces(this string param)  // this onemli 
+        {
+            return param.Contains(" ");
+        }
+
+        public static string RemoveWhiteSpaces(this string param)
+        {
+            string[] dizi = param.Split(" ");   // split ile bosluklarindan ayir dedik. 
+            return string.Join("", dizi);       // join ile bosluksuz dizi haline getirdik
+            // return string.Join("*", dizi);       // join ile bosluklar yerine * ekledik diziye
+           
+        }
+
+        public static string MakeUpperCase(this string param)
+        {
+            return param.ToUpper();
+        }
+
+        public static string MakeLowerCase(this string param)
+        {
+            return param.ToLower();
+        }
+
+        public static int[] SortArray(this int[] param)
+        {
+            Array.Sort(param);
+            return param;
+        }
+
+        public static void EkranaYazdir(this int[] param)
+        {
+            foreach (int item in param)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static bool IsEvenNumber(this int param)
+        {
+            return param % 2 == 00;
+        }
+
+        public static string GetFirstCharacter(this string param)
+        {
+            return param.Substring(0, 1);
+        }
+    }
+}
