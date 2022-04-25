1a515828b5963dcd7391513398e11e96f8f0c1ab
korkutluata <korkutkutluata@gmail.com>
2022-04-25 16:18:58 +0300

    ToDo console app odev proje eklendi

---
+++ C#_Odevler/ToDo_App_Console/CardSize.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ToDo_App_Console
+{
+    public enum CardSize
+    {
+        XS=1,
+        S,
+        M,
+        L,
+        XL
+    }
+}
---
+++ C#_Odevler/ToDo_App_Console/Line.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace ToDo_App_Console
+{
+    public enum Line
+    {
+        TODOLine = 1,
+        INPROGRESSLine,
+        DONELİNE
+    }
+}
